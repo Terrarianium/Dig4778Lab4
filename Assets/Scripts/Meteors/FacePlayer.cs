dd5a1b735949d154681d442e56fa7edb787d1428
Terrarianium <Collinrogers915@gmail.com>
2025-09-21 20:49:27 -0400

    Lab 3 Movement

---
+++ Assets/Scripts/Meteors/FacePlayer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TreeEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class FacePlayer : MonoBehaviour
+{
+    // Variables
+    private GameObject player;
+    private Vector3 playerPosition;
+
+    // Set player
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Get angle between enemy and player then rotate
+    void Update()
+    {
+        Vector3 directionDifferent = player.transform.position - transform.position;
+        float signedAngle = Vector3.SignedAngle(transform.up, directionDifferent, Vector3.forward);
+        Vector3 rotation = new Vector3(0, 0, signedAngle + 90);
+
+        transform.Rotate(rotation * Time.deltaTime * 10);
+    }
+}
